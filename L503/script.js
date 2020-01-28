b9a63de52443a95f140a72900636773f318d757a
BUGMAN-ini <56508989+BUGMAN-ini@users.noreply.github.com>
2020-01-28 16:45:31 +0400

    lab 5

---
+++ L503/script.js
@@ -0,0 +1,10 @@
+let table = document.querySelector("#table");
+
+function diagonalBackground(table, color) {
+    for (let i = 0; i < table.rows.length; i++) {
+        let cell = table.rows[i].cells[i];
+        cell.style.backgroundColor = color;
+    }
+}
+
+diagonalBackground(table, "red");
---
+++ L504/script.js
@@ -0,0 +1,5 @@
+let phones = document.querySelectorAll(".model");
+
+for (let i = 0; i < phones.length; i++) {
+    console.log(phones[i].textContent);
+};
